c672a06dd327c49e4bb5de1e88bd67aadb349557
EvaFro <frolli.erin@gmail.com>
2018-01-16 21:53:33 -0800

    Exicuted Extend & tests

--- src/underbar.js
+++ src/underbar.js
@@ -259,6 +259,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var args = arguments;
+    _.each(args, function(item) {
+      Object.assign(obj, item);
+    });
+    return obj;
+  
   };
 
   // Like extend, but doesn't ever overwrite a key that already
